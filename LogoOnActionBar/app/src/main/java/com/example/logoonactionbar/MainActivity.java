8b39d9636af0c7e8c513f6f96a52b782b715412e
Jannatul Ferdoush <jannatferdoush1212@gmail.com>
2021-06-14 11:55:25 +0600

    LogoOnActionBar

---
+++ LogoOnActionBar/app/src/main/java/com/example/logoonactionbar/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.logoonactionbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(R.mipmap.ic_launcher);//any image can useable
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+    }
+}
